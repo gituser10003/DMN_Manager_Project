65a0d304259afd49294d0415e5e385a94e431fee
shinwonseok1 <vhdlwms910910@gmail.com>
2022-09-23 17:43:43 +0900

    11

--- Manager_Project/src/main/java/com/controller/MainController.java
+++ Manager_Project/src/main/java/com/controller/MainController.java
@@ -1,9 +1,10 @@
 package com.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.service.SawonService;
 
 @Controller
@@ -11,14 +12,9 @@ public class MainController {
 @Autowired
 SawonService service;
 
-//@RequestMapping("/") 
-//public ModelAndView sawonList() {
-//	  List<SawonDTO> list= service.sawonList("top");
-//	  ModelAndView mav= new ModelAndView();
-//	  mav.addObject("sawonList", list);
-//	  //request.setAttribute("goodsList", list)와 동일
-//	  mav.setViewName("main");//main.jsp
-//	  return mav;	  
+//@RequestMapping("/")
+//public String main() {
+//		return "main";
 //}
 
 }
