0a26a40d8d62f01458da23456736c350b0285d85
gituser10003 <githan1203@gmail.com>
2022-09-23 18:13:50 +0900

    mybatis list 불러오기 확인용

--- Manager_Project/src/main/java/com/dao/SawonDAO.java
+++ Manager_Project/src/main/java/com/dao/SawonDAO.java
@@ -1,11 +1,19 @@
 package com.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.dto.SawonDTO;
+
 @Repository
 public class SawonDAO {
 	@Autowired
-	SqlSessionTemplate template;
+	SqlSessionTemplate session;
+
+	public List<SawonDTO> list() {
+		return session.selectList("SawonMapper.list");
+		}
 }
