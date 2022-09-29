dbe7c0b5cef560333e08f8c397e723285cfa2a19
gituser10003 <110511248+gituser10003@users.noreply.github.com>
2022-09-29 17:28:08 +0900

    Update SawonService.java

--- Manager_Project/src/main/java/com/service/SawonService.java
+++ Manager_Project/src/main/java/com/service/SawonService.java
@@ -4,12 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.dao.SawonDAO;
-import com.dto.SawonDTO;
-
-@Service
-public class SawonService {
+	@@ -14,9 +13,15 @@ public class SawonService {
 @Autowired
 SawonDAO dao;
 
@@ -20,8 +15,4 @@ public void sawonAdd(SawonDTO s) {
 public SawonDTO login(Map<String, String> map) {
 	SawonDTO dto = dao.login(map);
 	return dto;
-}
-
-
-
 }
